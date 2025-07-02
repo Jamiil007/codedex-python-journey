e860fb07cf6942cb2bc2091a5f0d1a8819808e00
Jamiil007 <oussama.jamil.11@gmail.com>
2025-07-02 12:59:34 +0100

    More Python

---
+++ 1-hello-world/04_initials.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+# Fun Fact: I love solving puzzles and can solve a Rubik's cube in under 2 minutes!
+
+print("DDDDD      L")
+print("D    D     L")
+print("D     D    L")
+print("D     D    L")
+print("D     D    L")
+print("D    D     L")
+print("DDDDD      LLLLL")
