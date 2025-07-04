c1ab77c596f5c5c9db4a6966ee1c2a9d5e32eb7a
Jamiil007 <oussama.jamil.11@gmail.com>
2025-07-04 19:01:12 +0100

    New challenge

---
+++ 2-variables/8-bmi.py
@@ -0,0 +1,9 @@
+    # write your code below 💖
+    # This program calculates the Body Mass Index (BMI) using the formula:
+
+    weight = 92.3
+    height = 1.86
+
+    bmi = weight / (height ** 2)
+
+    print(bmi)
