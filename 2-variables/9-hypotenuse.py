4cb9ca219f85cf9950af5c637236794e8dd14637
Jamiil007 <oussama.jamil.11@gmail.com>
2025-07-09 11:40:05 +0100

    Pythagorean Theorem 📐

---
+++ 2-variables/9-hypotenuse.py
@@ -0,0 +1,11 @@
+# write your code below 💖
+# 📐 This program calculates the hypotenuse (c) of a right triangle
+# using the Pythagorean Theorem: c = √(a² + b²)
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+c = (a ** 2 + b ** 2) ** 0.5
+
+print(c)
